f2f67f291a6dd02ccc674294a63c43919b03211b
RAWSIN <diveshkumarsingariya@gmail.com>
2022-11-09 11:54:13 +0530

    firebase modified

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
+import {getStorage} from 'firebase/storage'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXRMcd44Kqgcwv2wikLBkSkdVo5vgqOTI",
-  authDomain: "webchat-d8dca.firebaseapp.com",
-  projectId: "webchat-d8dca",
-  storageBucket: "webchat-d8dca.appspot.com",
-  messagingSenderId: "232590667453",
-  appId: "1:232590667453:web:0676bde721cf3eb35ed1d4",
-  measurementId: "G-F4M0MYGSMW"
+  apiKey: "AIzaSyCrMKjsBAE8FdhM6RiWFOTX8vGxFCL2bY4",
+  authDomain: "webchat-96792.firebaseapp.com",
+  projectId: "webchat-96792",
+  storageBucket: "webchat-96792.appspot.com",
+  messagingSenderId: "842953549883",
+  appId: "1:842953549883:web:417d283f2cafb13ce6d41e",
+  measurementId: "G-5V3HNWYWQF"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const Auth = getAuth(app)
+export const auth = getAuth()
+export const storage = getStorage()
+export const db = getFirestore(app)
+
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,76 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebaseConfig";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+import { doc, setDoc } from "firebase/firestore";
 
 function Register() {
-  const [formData,setFormData] = useState({username:"",email:"",password:'',file:""})
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState("");
 
-  function handleChange(e){
-    const {name,value} = e.target
-    setFormData(prevState => ({...prevState,[name]:value}))
+  function handleFileChange(e) {
+    e.preventDefault();
+    const fileRef = e.target.files[0];
+    setFile(fileRef);
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(formData)
-    setFormData({username:"",email:"",password:""})
-    
+  function handleChange(e) {
+    e.preventDefault();
+    setLoading(true);
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
   }
 
- 
-   
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await createUserWithEmailAndPassword(
+        auth,
+        formData.email,
+        formData.password
+      );
+      const storageRef = ref(storage, formData.username);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (err) => setError(err.message),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            try {
+            await updateProfile(res.user, {
+              displayName: formData.username,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName: formData.username,
+              email: formData.email,
+              photoURL: downloadURL,
+            });
+              await setDoc(doc(db, "userChats",res.user.uid),{});
+            }
+            catch (err){
+              setError(err.message)
+            
+            }
+          });
+        }
+      );
+            // 
+      // setFormData({ username: "", email: "", password: "" });
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="w-screen  h-screen flex items-center justify-center bg-gradient-to-b from-purple-500 to-pink-500">
@@ -25,7 +79,8 @@ function Register() {
         <h4 className="mb-4 ">Register</h4>
         <form onSubmit={handleSubmit} className="flex flex-col  w-2/4 gap-4">
           <input
-            name='username'
+            name="username"
+            value={formData.username}
             onChange={handleChange}
             className="border-2 border-gray-600 rounded-sm px-2 py-1"
             type="text"
@@ -33,6 +88,7 @@ function Register() {
           />
           <input
             name="email"
+            value={formData.email}
             onChange={handleChange}
             className="border-2 w-full border-gray-600 rounded-sm px-2 py-1"
             type="email"
@@ -40,21 +96,29 @@ function Register() {
           />
           <input
             name="password"
+            value={formData.password}
             onChange={handleChange}
             className="border-2 border-gray-600 rounded-sm px-2 py-1"
             type="password"
             placeholder="password"
           />
-          <input className="hidden" type="file" name="file" onChange={handleChange}  id="file"/>
+          <input
+            className="hidden"
+            type="file"
+            name="file"
+            onChange={handleFileChange}
+            id="file"
+          />
           <label htmlFor="file">
             <div className="flex items-center gap-2">
-            <img src="images/picture.png" alt="upload"/>
-            <p>Add an avatar</p>
+              <img src="images/picture.png" alt="upload" />
+              <p>Add an avatar</p>
             </div>
           </label>
           <button className="bg-gray-800 text-white px-8 py-1">Sign up</button>
         </form>
         <p className="my-4">Already have an account? Login</p>
+        {error && <p className="text-red-500 my-2">{error}</p>}
       </div>
     </div>
   );
